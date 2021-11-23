4e73bf40842e49ea492b90c9eb8a1b570de6ab14
programer10 <programer10@ucione.local>
2021-11-23 19:17:34 +0100

    stage2

--- src/vj2311/ShowCount.js
+++ src/vj2311/ShowCount.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export default function ShowCount({ totalCount, count3 }) {
+export default function ShowCount({ totalCount, count3, last }) {
     return (<div>
                 <p>Count: {totalCount}</p>
                 <p>3 count: {count3}</p>
+                <p>Last divisible by 3: {last}</p>
             </div>);
 }
--- src/vj2311/Vj2311.js
+++ src/vj2311/Vj2311.js
@@ -5,23 +5,28 @@ import ShowCount from './ShowCount';
 export default class Vj2311 extends React.Component {
     state = {
         total: 0,
-        count3: 0
+        count3: 0,
+        lastDivisible: 0
     }
 
     makeAdd = () => {
         const newTotal = this.state.total + 1;
         let newCount3 = this.state.count3;
+        let newLastDivisible = this.state.lastDivisible;
 
         if(newTotal % 3 === 0) {
             newCount3++;
+            newLastDivisible=newTotal;
         }
 
-        this.setState({ total: newTotal, count3: newCount3});
+        this.setState({ total: newTotal, count3: newCount3, lastDivisible: newLastDivisible});
     }
 
     render() {
         return (<div>
-                    <ShowCount totalCount={this.state.total} count3={this.state.count3} />
+                    <ShowCount totalCount={this.state.total} 
+                                count3={this.state.count3} 
+                                last={this.state.lastDivisible} />
                     <FensiButton onBtnClick={this.makeAdd}>ADD</FensiButton>
                 </div>);
     };   
